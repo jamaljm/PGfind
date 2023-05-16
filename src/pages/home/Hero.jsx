0e871933c2ab3785c415258e397630c6955d05a7
jam-dev <jamalvga2002@gmail.com>
2023-05-16 10:49:00 +0530

    landing page done

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import heroimg from "../../images/hero.jpg";
+import logo from '../../images/logo.png'
+import './hero.css'
 
 export default function Hero() {
   return (
     <div className="hero">
       <div className="nav">
         <div className="nav-left">
-          <h1>PGfind</h1>
+          <img src={logo} alt=""></img>
         </div>
         <div className="nav-right">
           <a href="/login">Login</a>
@@ -15,7 +17,10 @@ export default function Hero() {
       <div className="hero-cont">
         <div className="hero-left">
           <h1>Discover your dream PG today</h1>
-          <button>Join Now</button>
+
+         
+            <a href="/login">Join Now</a>
+       
         </div>
         <div className="hero-right">
           <img src={heroimg} alt=""></img>
