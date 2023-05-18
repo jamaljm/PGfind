c0def248a9c4cf70cb38bb52c6e8ee18bb158ba2
jam-dev <jamalvga2002@gmail.com>
2023-05-18 18:44:41 +0530

    dashboard structure

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
+import "./Dashboard.css";
+import logo from '../../images/logo.png'
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <div className="dashboard">
+      <div className="nav">
+        <div className="nav-left">
+          <img src={logo} alt=""></img>
+        </div>
+        <div className="nav-right">
+          <p>name</p>
+          <img src="" alt=""></img>
+        </div>
+      </div>
+      <div className="dashboard-cont">
+        <div className="side-bar"></div>
+        <div className="main-cont"></div>
+        <div className="right-bar"></div>
+      </div>
+    </div>
+  );
 }
