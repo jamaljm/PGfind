35d4aa83e07f1113a2f7b76a7ce6b47d2ddcf751
jam-dev <jamalvga2002@gmail.com>
2023-05-18 21:10:12 +0530

    Authcontext done

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,24 +1,31 @@
-import React from "react";
+import React,{ useContext } from "react";
 import "./Dashboard.css";
 import logo from '../../images/logo.png'
+import { AuthContext } from "../../context";
+
 
 export default function Dashboard() {
-  return (
-    <div className="dashboard">
-      <div className="nav">
-        <div className="nav-left">
-          <img src={logo} alt=""></img>
+  const { user } = useContext(AuthContext);
+  console.log(user);
+
+  if (user) {
+    return (
+      <div className="dashboard">
+        <div className="nav">
+          <div className="nav-left">
+            <img src={logo} alt=""></img>
+          </div>
+          <div className="nav-right">
+            <p>{user.displayName}</p>
+            <img src={user.photoURL} alt=""></img>
+          </div>
         </div>
-        <div className="nav-right">
-          <p>name</p>
-          <img src="" alt=""></img>
+        <div className="dashboard-cont">
+          <div className="side-bar"></div>
+          <div className="main-cont"></div>
+          <div className="right-bar"></div>
         </div>
       </div>
-      <div className="dashboard-cont">
-        <div className="side-bar"></div>
-        <div className="main-cont"></div>
-        <div className="right-bar"></div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
